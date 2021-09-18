7ac8d7b28b21a71b87a0d516457c1b44bc70ada3
lollipop-onl <lollipop@simochee.net>
2021-09-18 15:33:52 +0900

    feat: setup Firebase

--- src/pages/admin/conte/list.tsx
+++ src/pages/admin/conte/list.tsx
@@ -1,10 +1,14 @@
 import React from "react"
+import Link from "next/link";
 import { AdminLayout } from "~/components/Admin/AdminLayout";
 
 const AdminConteList: React.VFC = () => {
   return (
     <AdminLayout>
       <h1>コント一覧</h1>
+      <Link href="/admin/conte/new">
+        <a>コントを登録</a>
+      </Link>
     </AdminLayout>
   )
 }
